4b44fea111dd0e7089114b7a4b2538068d0e0b30
MozharAlhosni <pedant@hackthebox.com>
2025-12-13 00:29:15 +0000

    migrate to mudblazor

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using mozharsgarden;
-using Radzen;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddRadzenComponents();
+
+builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 
 builder.Services.AddScoped(serviceType => new HttpClient
 {

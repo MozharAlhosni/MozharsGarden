c819cf06185c6221d12d9e090df523fb46279a33
MozharAlhosni <pedant@hackthebox.com>
2026-04-15 08:47:36 +0000

    update d.g.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 			[
 				{
 					label: 'Consultation Services',
-					link: 'Consultation_Services'
+					link: '/Consultation_Services'
 				},
 				{
 					label: 'Computer Science',
